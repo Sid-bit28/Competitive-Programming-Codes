5bacd699fddd48938f6ce78db290cee3bc77fcf3
Siddarth <siddarth2801@gmail.com>
2022-05-24 20:59:53 +0530

    Random stuffs

---
+++ SPOJ/SPOJ BITMAP.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma comment(linker, "/stack:200000000")
+using namespace std;
+#define F first
+#define S second
+#define EB emplace_back
+#define MP make_pair
+#define all(o) (o).begin(), (o).end()
+#define mset(m,v) memset(m,v,sizeof(m))
+#define fr(i,n) for(lli i=0;i<(n);++i)
+#define rep(i,a,b) for(lli i=a;i<=b;++i)
+#define per(i,a,b) for(lli i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b))) 
+#define sz(x) (lli)(x).size()
+#define endl '\n'
+typedef long long lli;         typedef long double ld;
+typedef pair<lli,lli> pii;     typedef vector<lli> vi;
+typedef vector<pii> vii;       typedef vector<vi> graph;
+const long long MOD=1000000007;const long long INF=1e18;
+#ifndef SIDDARTH
+#define debarr(a,n) cerr<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debmat(mat,row,col) cerr<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+#else
+#define pr(...) {}
+#define debarr(a,n) {}
+#define debmat(mat,row,col) {}
+#endif
+lli binpow(lli b,lli p,lli mod){lli ans=1;b%=mod;for(;p;p>>=1){if(p&1)ans=ans*b%mod;b=b*b%mod;}return ans;}
+
+const int mxN=200;
+lli n,m;
+vector<string> s(mxN);
+vector<vector<lli>> dis(mxN,vector<lli>(mxN,1e9));
+int dx[]={1,0,-1,0};
+int dy[]={0,1,0,-1};
+void pre(){
+
+}
+bool check(int x,int y){
+	if(x>=0&&x<n&&y>=0&&y<m&&s[x][y]=='0')return 1;
+	return 0;
+}
+void solve(){
+	cin>>n>>m;
+	fr(i,n)cin>>s[i];
+	dis= vector<vector<lli>> (n,vector<lli>(m,1e9));
+	queue<pii> q;
+	fr(i,n){
+		fr(j,m){
+			if(s[i][j]=='1'){
+				q.push({i,j});
+				dis[i][j]=0;
+			}
+		}
+	}
+	while(!q.empty()){
+		pii pos=q.front();
+		q.pop();
+		fr(i,4){
+			if(check(pos.F+dx[i],pos.S+dy[i])&&dis[pos.F+dx[i]][pos.S+dy[i]]>dis[pos.F][pos.S]+1){
+				dis[pos.F+dx[i]][pos.S+dy[i]]=dis[pos.F][pos.S]+1;
+				q.push({pos.F+dx[i],pos.S+dy[i]});
+			}
+		}
+	}
+	fr(i,n){
+		fr(j,m){
+			cout<<dis[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    pre();lli _t=1;cin>>_t;
+    for(lli i=1;i<=_t;i++){
+        //cout<<"Case #"<<i<<": ";
+        solve();
+    }
+}
